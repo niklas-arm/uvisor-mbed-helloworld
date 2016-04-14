ad76eea5eaf1083f0be9501d48cf3d8424532255
Niklas Hauser <niklas.hauser@arm.com>
2016-04-14 15:40:39 +0100

    pages.

--- source/main.cpp
+++ source/main.cpp
@@ -21,6 +21,8 @@
 #include "box-debug.h"
 #include "btn.h"
 
+const unsigned int kB = 1024;
+
 /* Create ACLs for main box. */
 MAIN_ACL(g_main_acl);
 
@@ -46,11 +48,57 @@ static bool retry_secret(void)
     return verified;
 }
 
+static void dump(const UvisorPageTable *const table)
+{
+    int table_size = table->page_count;
+    if (!table_size || !table->page_size) {
+        printf("pages_dump: FAIL: Page table empty!\n");
+        return;
+    }
+    printf("pages_dump: Table with %d pages of size %ukB\n", table_size, table->page_size/kB);
+    for (void* const *page = table->page_origins; table_size > 0 && *page; page++, table_size--)
+    {
+        printf("pages_dump: Page #%u at %p\n", page - table->page_origins, *page);
+    }
+    printf("\n");
+}
+
 int main(void)
 {
     bool verified;
+    extern serial_t stdio_uart;
+    serial_init(&stdio_uart, STDIO_UART_TX, STDIO_UART_RX);
+    serial_baud(&stdio_uart, 115200);
 
     printf("\r\n***** uvisor-helloworld example *****\r\n");
+
+    /* I want to see some page fragmentation happening here */
+    /* array for keeping track of page tables */
+    UVISOR_PAGE_TABLE(7) request[10];
+    /* allocate 1 page */
+    request[0].page_size = UVISOR_PAGE_SIZE;
+    request[0].page_count = 1;
+    uvisor_page_malloc((UvisorPageTable*)&request[0]); dump((UvisorPageTable*)&request[0]);
+    /* allocate 1 page */
+    request[1].page_size = UVISOR_PAGE_SIZE;
+    request[1].page_count = 1;
+    uvisor_page_malloc((UvisorPageTable*)&request[1]); dump((UvisorPageTable*)&request[1]);
+    /* allocate 2 pages */
+    request[2].page_size = UVISOR_PAGE_SIZE;
+    request[2].page_count = 2;
+    uvisor_page_malloc((UvisorPageTable*)&request[2]); dump((UvisorPageTable*)&request[2]);
+    /* allocate 1 page */
+    request[3].page_size = UVISOR_PAGE_SIZE;
+    request[3].page_count = 1;
+    uvisor_page_malloc((UvisorPageTable*)&request[3]); dump((UvisorPageTable*)&request[3]);
+    /* allocate 2 page */
+    request[4].page_size = UVISOR_PAGE_SIZE;
+    request[4].page_count = 1;
+    uvisor_page_malloc((UvisorPageTable*)&request[4]); dump((UvisorPageTable*)&request[4]);
+    /* free 1 page */
+    uvisor_page_free((UvisorPageTable*)&request[1]);
+    /* free 1 page */
+    uvisor_page_free((UvisorPageTable*)&request[3]);
 
     /* Initialize the debug box. */
     box_debug::init();
